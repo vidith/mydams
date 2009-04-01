da82098ffcf35117f4400e9e055410e7f05b2b81
deepak@deepak-patil.geodesic.net <deepak@deepak-patil.geodesic.net>
2009-04-01 17:17:51 +0530

    UI Added

--- system/application/views/welcome_message.php
+++ system/application/views/welcome_message.php
@@ -1,62 +1,80 @@
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>Welcome to CodeIgniter</title>
-
-<style type="text/css">
-
-body {
- background-color: #fff;
- margin: 40px;
- font-family: Lucida Grande, Verdana, Sans-serif;
- font-size: 14px;
- color: #4F5155;
-}
-
-a {
- color: #003399;
- background-color: transparent;
- font-weight: normal;
-}
-
-h1 {
- color: #444;
- background-color: transparent;
- border-bottom: 1px solid #D0D0D0;
- font-size: 16px;
- font-weight: bold;
- margin: 24px 0 2px 0;
- padding: 5px 0 6px 0;
-}
-
-code {
- font-family: Monaco, Verdana, Sans-serif;
- font-size: 12px;
- background-color: #f9f9f9;
- border: 1px solid #D0D0D0;
- color: #002166;
- display: block;
- margin: 14px 0 14px 0;
- padding: 12px 10px 12px 10px;
-}
-
-</style>
+	<title>MYDAMS - My Digital Assets Manegment Systems</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link href="system/application/media/css/style.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
+  <div id="wrapper">
+      <h1><a href="http://deepak-patil.geodesic.net/~deepak/mydams/"><img src="system/application/media/images/logo.gif"  alt="Travel Agency" /></a>Digital Assets Mgmt System</h1>
+      <div id="booking">
+          <h2>Assets Search</h2>
+            <form action="?" method="get">
+              <div class="jtype">
+                  <label for="flight">Flight</label> <input name="flight" id="flight" type="checkbox" value="flight" /> &nbsp; &nbsp;
+                  <label for="hotel">Hotel</label> <input name="hotel" id="hotel" type="checkbox" value="hotel" /> &nbsp; &nbsp;
+                  <label for="cruise">Cruise</label> <input name="cruise" id="cruise" type="checkbox" value="cruise" />
+              </div><!-- end .jtype -->
+              <table summary="" cellspacing="0" cellpadding="0" border="0">
+                  <tr><th>Check in</th><td><input name="in" type="text" value="" class="text" /> <input type="submit" value="submit" class="submit" /></td></tr>
+                  <tr><th>Check out</th><td><input name="out" type="text" value="" class="text" /> <input type="submit" value="submit" class="submit" /></td></tr>
+              </table>
+              <p class="advanced"><a href="http://deepak-patil.geodesic.net/~deepak/mydams/"><img src="system/application/media/images/advanced.gif" width="11" height="11" alt="" /> Advanced Search</a></p>
+            </form>
+      </div><!-- end booking -->
+      <div id="nav">
+        <ul>
+          <li><a href="http://deepak-patil.geodesic.net/~deepak/mydams/">Home</a></li>
+          <li><a href="http://deepak-patil.geodesic.net/~deepak/mydams/">Tours</a></li>
+          <li><a href="http://deepak-patil.geodesic.net/~deepak/mydams/">Bookings</a></li>
+          <li><a href="http://deepak-patil.geodesic.net/~deepak/mydams/">Services</a></li>
+          <li><a href="http://deepak-patil.geodesic.net/~deepak/mydams/">Contacts</a></li>
+        </ul>
+      </div><!-- end nav -->
+      <h2  id="packagesheader"><img src="system/application/media/images/title_our_packages.gif" width="352" height="23" alt="our packages" /></h2>
+      <div id="packages">
 
-<h1>Welcome to CodeIgniter!</h1>
+          <h3 class="golden">Golden Package</h3>
+         <p>Don't forgot to check <a href="http://deepak-patil.geodesic.net/~deepak/mydams/">free website templates</a> every day, because we add at least one free website template daily.</p>
+				
+				<p>This is a template designed by free website templates for you for free you can replace all the text by your own 
+				text. This is just a place holder so you can see how the site would look like.</p> 
+          <p class="readmore"><a href="http://deepak-patil.geodesic.net/~deepak/mydams/"><img src="system/application/media/images/readmore.gif" width="68" height="15" alt="readmore" /></a></p>
 
-<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+          <h3 class="silveren">Silver Package</h3>
+         
+				
+				<p>If you're having problems editing the template please don't hesitate to ask for help on the <a href="http://deepak-patil.geodesic.net/~deepak/mydams//forum/">forum</a>.</p>
+          <p class="readmore"><a href="http://deepak-patil.geodesic.net/~deepak/mydams/"><img src="system/application/media/images/readmore.gif" width="68" height="15" alt="readmore" /></a></p>
 
-<p>If you would like to edit this page you'll find it located at:</p>
-<code>system/application/views/welcome_message.php</code>
-
-<p>The corresponding controller for this page is found at:</p>
-<code>system/application/controllers/welcome.php</code>
-
-<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-
-
-<p><br />Page rendered in {elapsed_time} seconds</p>
-
+          <div id="special">
+              <a href="http://deepak-patil.geodesic.net/~deepak/mydams/"><img src="system/application/media/images/ad_special_offer.gif" width="293" height="79" alt="special offer" /></a>
+          </div><!-- end special -->
+          
+      </div><!-- end packages -->
+      <div id="main">
+            <img src="system/application/media/images/biglogo.jpg" width="447" height="163" alt="two people having a walk" class="block" />
+            <h2><img src="system/application/media/images/title_hottest_locations.gif" width="447" height="24" alt="hottest locations" /></h2>
+          <div class="inner">
+            <h3 class="blue">Italy</h3>
+            <img src="system/application/media/images/photo_1.jpg" width="109" height="71" alt="stunning italian history" class="left" />
+             <p>You can remove any link to our websites from this template you're  free to use the template without linking 
+				back to us. Don't want your boss to know you used a free website template ;) .</p>
+				
+				<br /><br />
+            <h3 class="green">Hawai</h3>
+            <img src="system/application/media/images/photo_2.jpg" width="109" height="71" alt="sea, the beaches" class="left" />
+			<p>If you're looking for beautiful and professionally made templates you can find them at <a href="http://www.templatebeauty.com">Template Beauty</a>.</p>
+          <p>To find great hosting providers visit <a href="http://www.webhostingzoom.com">Web Hosting Zoom</a>.</p>
+          
+            <div class="clear"></div>
+          </div>
+          <div class="clear"></div>
+      </div><!-- end main -->
+      <div id="footer">
+          &copy; Copyright Information here
+      </div><!-- end footer -->
+  </div><!-- end wrapper -->
 </body>
 </html>
